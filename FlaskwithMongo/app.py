43661ac8920cb42ffc1f071b1e3e82181cae63b6
areeba-abid97 <82089068+areeba-abid97@users.noreply.github.com>
2022-06-14 21:28:32 +0500

    Add MongoDB Connection String Variables, Title and Heading

--- FlaskwithMongo/app.py
+++ FlaskwithMongo/app.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template,request,redirect,url_for
 from bson import ObjectId 
 from pymongo import MongoClient 
 import os
+
+#App Name
+app = Flask(__name__)
+
+title = "TODO application with Flask, MongoDB, and Angular"  
+heading = "TODO Application with Flask, MongoDB, and Angular"
+
+#MongoDB Connection String
+client = MongoClient("") #host uri  
+db = client.mymongodb #Select the database  
+todos = db.todo #Select the collection name   
     
 if __name__ == "__main__":    
     app.run()   
