b53998f27cadd0238a632d523b6bdfa0737198c7
areeba-abid97 <82089068+areeba-abid97@users.noreply.github.com>
2022-06-14 21:27:17 +0500

    Adding Imports and Main in app.py

---
+++ FlaskwithMongo/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template,request,redirect,url_for 
+from bson import ObjectId 
+from pymongo import MongoClient 
+import os
+    
+if __name__ == "__main__":    
+    app.run()   
