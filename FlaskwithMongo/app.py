142af10a4a27e1d1b4f5686d74242f7d8e43191f
areeba-abid97 <82089068+areeba-abid97@users.noreply.github.com>
2022-06-14 22:40:58 +0500

    API to add new tasks

--- FlaskwithMongo/app.py
+++ FlaskwithMongo/app.py
@@ -52,6 +52,16 @@ def done ():
     redir=redirect_url()        
     
     return redirect(redir)   
+
+#Adding a Task  
+@app.route("/action", methods=['POST']) #POST Application Route for Adding New Tasks   
+def action ():      
+    name=request.values.get("name")    
+    desc=request.values.get("desc")    
+    date=request.values.get("date")    
+    pr=request.values.get("pr")    
+    todos.insert({ "name":name, "desc":desc, "date":date, "pr":pr, "done":"no"})    
+    return redirect("/list")  
     
 if __name__ == "__main__":    
     app.run()   
